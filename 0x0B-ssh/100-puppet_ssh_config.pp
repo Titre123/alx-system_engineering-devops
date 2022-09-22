adcf40764a0799cde1a9a6dd757c51fbc74d165f
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-22 01:16:26 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,11 @@ include stdlib
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/holberton',
-  replace => true,
+  replace => true
 }
 
 file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
-  replace => true,
+  replace => true
 }
