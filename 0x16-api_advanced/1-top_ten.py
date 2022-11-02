76e82fc87695877b95146f75452fc693342ba803
taiwo <taiwotriumphant@gmail.com>
2022-11-01 21:30:44 -0700

    new

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,9 +2,8 @@
 """
 Query the Reddit API and return the top 10 post for a given subreddit
 """
-
-
 import requests
+
 
 def top_ten(subreddit):
     r = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'.
@@ -12,7 +11,7 @@ def top_ten(subreddit):
                      headers={'User-Agent': 'custom'},
                      allow_redirects=False)
     if (r):
-        children =  r.json().get('data').get('children')
+        children = r.json().get('data').get('children')
         for child in children:
             print(child.get('data').get('title'))
     else:
