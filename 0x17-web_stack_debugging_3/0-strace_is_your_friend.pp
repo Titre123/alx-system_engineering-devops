dafa89518eaca9fb2f471c964a1486d7a3cbe2bd
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-11-03 08:11:34 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # fix debugging problem
 exec { 'replace':
   command => "sed -i -e 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  return  => 0,
-  path    => '/usr/bin',
+  path    => 'bin',
 }
