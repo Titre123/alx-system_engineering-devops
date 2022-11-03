28611757f423f7d1b520e26698d045da85a375e9
taiwo <taiwotriumphant@gmail.com>
2022-11-03 00:14:00 -0700

    new

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix debugging problem
 exec { 'replace':
   command => "sed -i -e 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  path    => 'bin',
+  path    => '/bin',
 }
