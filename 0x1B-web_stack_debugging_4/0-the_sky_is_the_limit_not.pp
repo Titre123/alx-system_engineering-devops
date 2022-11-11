374c0405e25a24d81d09f785f5e5e63a8d1befee
Taiwo <taiwotriumphant@gmail.co>
2022-11-11 06:24:23 +0000

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
 # puppet file to fix multiple request problem
-
 exec { 'fix--for-nginx':
   command   => 'sed -i "s/15/4096/" /etc/default/nginx',
   provider  => shell,
@@ -10,4 +9,3 @@ exec { 'restart nginx':
   require   => Exec['fix--for-nginx'],
   provider  => shell,
 }
-
