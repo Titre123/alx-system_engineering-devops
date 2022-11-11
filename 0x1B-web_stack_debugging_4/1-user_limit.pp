c729dc3386b60da5b2e3801f54f09265e722d3f4
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-11-11 07:11:34 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # puppet file to enable holberton user
-user { 'holberton':
-    ensure   => 'present',
-    password => 'holberton',
+exec { 'change-os-configuration-for-holberton-user':
+  command  => 'sed -i "s/nofile 4/nofile 1024/" /etc/security/limits.conf',
+  provider => shell,
+}
+
+exec { 'change-os-configuration-for-holberton':
+  command => 'sed -i "s/nofile 5/nofile 2048/" /etc/security/limits.conf',
+  require => Exec['change-os-configuration-for-holberton-user'],
+  provider => shell,
 }
