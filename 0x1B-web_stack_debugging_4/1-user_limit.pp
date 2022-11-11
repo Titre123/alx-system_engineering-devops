9e568baeee550f9c61eac3424503029d9aaaf96a
Taiwo <taiwotriumphant@gmail.co>
2022-11-11 06:17:54 +0000

    new

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,12 @@
-# puppet file to enable holberton user
+# Puppet file to confiure user holberton limits
+
 exec { 'change-os-configuration-for-holberton-user':
   command  => 'sed -i "s/nofile 4/nofile 1024/" /etc/security/limits.conf',
   provider => shell,
 }
 
 exec { 'change-os-configuration-for-holberton':
-  command => 'sed -i "s/nofile 5/nofile 2048/" /etc/security/limits.conf',
-  require => Exec['change-os-configuration-for-holberton-user'],
+  command  => 'sed -i "s/nofile 5/nofile 2048/" /etc/security/limits.conf',
+  require  => Exec['change-os-configuration-for-holberton-user'],
   provider => shell,
 }
